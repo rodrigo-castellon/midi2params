382810e640d3af4866ee8cd8b170cc26290c612f
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2020-10-07 15:01:11 -0400

    added files

---
+++ download_links.py
@@ -0,0 +1,29 @@
+# download a bunch of files from the user study into the github
+# repo to avoid weird SSL cert (?) errors
+
+import os
+import urllib.request
+
+link_body = 'https://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/'
+clip_nums = [
+    8,
+    10,
+    12,
+    16,
+    17
+]
+
+clip_types = ['obligation', 'shoulder', 'grandmother', 'art', 'variant', 'bubble']
+
+
+for num in clip_nums:
+    for tp in clip_types:
+        pointer = os.path.join(tp, str(num).zfill(2) + '.wav')
+        link = os.path.join(link_body, pointer)
+        print(link)
+        local_link = os.path.join('data', pointer)
+        local_dir = os.path.split(local_link)[0]
+        if not os.path.exists(local_dir):
+            os.makedirs(local_dir)
+        urllib.request.urlretrieve(link, local_link)
+
