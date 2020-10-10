786fee5fa89687b4164d88fcc0bdcff7ded0be15
rodrigo-castellon <rodrigo.castellon@pinecrest.edu>
2020-10-09 23:32:22 -0400

    adjusted the naming to match the paper

--- create_table.py
+++ create_table.py
@@ -9,6 +9,9 @@ s_str = """<tr>
     <td><audio controls="controls" src="http://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/obligation/00.wav">
         Your browser does not support the HTML5 Audio element.
     </audio></td>
+    <td><audio controls="controls" src="http://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/variant/00.wav">
+        Your browser does not support the HTML5 Audio element.
+    </audio></td>
     <td><audio controls="controls" src="http://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/shoulder/00.wav">
         Your browser does not support the HTML5 Audio element.
     </audio></td>
@@ -18,21 +21,26 @@ s_str = """<tr>
     <td><audio controls="controls" src="http://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/art/00.wav">
         Your browser does not support the HTML5 Audio element.
     </audio></td>
+    <td><audio controls="controls" src="http://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/drain/00.wav">
+        Your browser does not support the HTML5 Audio element.
+    </audio></td>
     <td><audio controls="controls" src="http://nlp.stanford.edu/data/cdonahue/wavegenie_userstudy/v2/bubble/00.wav">
         Your browser does not support the HTML5 Audio element.
     </audio></td>
 </tr>"""
 
-def mod_string(i):
+def mod_string(i, idx):
+    # i: clip number from the dataset
+    # idx: clip number on the website
     global s_str
 
     s = copy.deepcopy(s_str)
     for m in re.finditer('\.wav', s):
         s = s[:m.start() - 2] + str(i).zfill(2) + s[m.start():]
     for m in re.finditer('##', s):
-        s = s[:m.start()] + str(i).zfill(2) + s[m.start() + 4:]
+        s = s[:m.start()] + str(idx) + s[m.start() + 4:]
     print(s)
 
 if __name__ == '__main__':
-    for i in range(19):
-        mod_string(i)
+    for idx, i in enumerate([8, 10, 12, 16, 17]):
+        mod_string(i, idx + 1)
